dce384acde5091e468e0ea0a3b0117c2ac6348d3
Paramjitsingh21 <paramjitsaini70@gmail.com>
2021-04-17 01:20:28 +0530

    Added Side Menu bar

--- content/static/js/main.js
+++ content/static/js/main.js
@@ -1,40 +1,6 @@
+$(document).ready(function () {
+  $('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+  });
 
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").classList.add("active");
-    document.getElementsByClassName("dropdown-content")[0].classList.add("dropdown-hower");
-  } else {
-    document.getElementById("header").classList.remove("active");
-    document.getElementsByClassName("dropdown-content")[0].classList.remove("dropdown-hower");
-    
-  }
-}
-
-
-
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("reset-btn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+});
--- paramPost/urls.py
+++ paramPost/urls.py
@@ -21,7 +21,6 @@ from accounts.views import frontpage
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexPageView.as_view(), name='index'),
-    path('home', IndexPageView.as_view(), name='home'),
     path('about',AboutPageView.as_view(), name='about'),
     path('',include('accounts.urls')),
 ]
